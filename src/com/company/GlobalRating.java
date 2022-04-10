06ac6117395b6e5cce9cc0126e0a618b4d979a67
Maksym Pinchuk <maxpinchukrv@gmail.com>
2022-04-10 18:02:17 +0300

    Classes

---
+++ src/com/company/GlobalRating.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class GlobalRating {
+
+    public static void main(String[] args) {
+        Score[] data = new Score[4];
+    }
+}
---
+++ src/com/company/Score.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Score {
+
+    String category;
+
+    static double[] statistic = new double[10];
+
+    {
+        statistic = new double[10];
+        for (int i = 0; i < 10; i++) {
+            statistic[i] = i * 1;
+        }
+    }
+}
